d68139a3ca18aa7660ac64b128382384b7ceb01a
pickar <sereges2@gmail.com>
2022-05-12 19:21:56 +0300

    Add simple math

---
+++ src/SimpleMath/Test.cs
@@ -0,0 +1,18 @@
+﻿using SimpleMath.Vectors;
+
+var vec3F = new Vector3<float>(5, 1, 3);
+
+Console.WriteLine(vec3F * 4.0);
+Console.WriteLine(vec3F.Add(3L).Sub(0f, -.5f, 1f).Add(vec3F).Add((-2.1, 3.1, 0d)));
+Console.WriteLine(vec3F * 2L);
+Console.WriteLine((Vector3<long>) vec3F * 5.3);
+
+var vec4D = new Vector4<double>(2.4, 5.1, 0, 0);
+var otherVec4D = new Vector4<double>(0.5);
+var testVec4D = new Vector4<double>(100);
+
+Console.WriteLine(vec4D - (2d, 3d, 1, 1) + otherVec4D);
+Console.WriteLine(otherVec4D);
+Console.WriteLine(vec4D.Set(otherVec4D).Set((5, 5, 5, 5)).Set(0.4, 0.1, 0.4, 0.1).Set(0.2).Put(ref otherVec4D));
+Console.WriteLine(testVec4D);
+Console.WriteLine(vec4D.Ceil(ref testVec4D));
---
+++ src/SimpleMath/Vectors/Vector2.cs
@@ -0,0 +1,102 @@
+﻿namespace SimpleMath.Vectors;
+
+public static class Vector2Extensions
+{
+	public static Vector2<T> Rotated90DegClockwise<T>(this Vector2<T> vector) where T : struct, INumber<T> => new(vector.Y, -vector.X);
+
+	public static ref Vector2<T> Rotate90DegClockwise<T>(this ref Vector2<T> vector) where T : struct, INumber<T> => ref vector.Set(vector.Y, -vector.X);
+
+	public static double Dot<T, TOther>(this Vector2<T> vector, Vector2<TOther> other)
+		where T : struct, INumber<T> where TOther : struct, INumber<TOther> =>
+		Double.Create(vector.X * T.Create(other.X) + vector.Y * T.Create(other.Y));
+
+	public static double Dot<T, TOther>(this Vector2<T> vector, (T X, T Y) other)
+		where T : struct, INumber<T> where TOther : struct, INumber<TOther> =>
+		Double.Create(vector.X * T.Create(other.X) + vector.Y * T.Create(other.Y));
+	
+	public static ref Vector2<T> Min<T, TOther>(this ref Vector2<T> vector, Vector2<TOther> other)
+	where T : struct, INumber<T> where TOther : struct, INumber<TOther> => ref Min(vector, other, ref vector);
+
+	public static ref Vector2<T> Min<T, TOther>(this Vector2<T> vector, Vector2<TOther> other, ref Vector2<T> dest)
+	where T : struct, INumber<T> where TOther : struct, INumber<TOther>
+	{
+		var otherX = T.Create(other.X);
+		var otherY = T.Create(other.Y);
+		
+		dest.X = vector.X < otherX ? vector.X : otherX;
+		dest.Y = vector.Y < otherY ? vector.Y : otherY;
+
+		return ref dest;
+	}
+	
+	public static Vector2<T> Min<T, TOther>(Vector2<T> vector, Vector2<TOther> other)
+	where T : struct, INumber<T> where TOther : struct, INumber<TOther>
+	{
+		var otherX = T.Create(other.X);
+		var otherY = T.Create(other.Y);
+
+		return new Vector2<T>(vector.X < otherX ? vector.X : otherX, vector.Y < otherY ? vector.Y : otherY);
+	}
+	
+	public static ref Vector2<T> Max<T, TOther>(this ref Vector2<T> vector, Vector2<TOther> other)
+	where T : struct, INumber<T> where TOther : struct, INumber<TOther> => ref Max(vector, other, ref vector);
+
+	public static ref Vector2<T> Max<T, TOther>(this Vector2<T> vector, Vector2<TOther> other, ref Vector2<T> dest)
+	where T : struct, INumber<T> where TOther : struct, INumber<TOther>
+	{
+		var otherX = T.Create(other.X);
+		var otherY = T.Create(other.Y);
+		
+		dest.X = vector.X > otherX ? vector.X : otherX;
+		dest.Y = vector.Y > otherY ? vector.Y : otherY;
+
+		return ref dest;
+	}
+
+	public static bool IsInside<T>(this Vector2<T> point, Vector2<T> topLeft, Vector2<T> bottomRight) where T : struct, INumber<T> =>
+		point.X >= topLeft.X && point.X < bottomRight.X && point.Y >= topLeft.Y && point.Y < bottomRight.Y;
+
+	// public static ref Vector2<T> Max<T, TOther>(this ref Vector2<T> vector, Vector2<TOther> other)
+	// where T : struct, INumber<T> where TOther : struct, INumber<TOther>
+	// {
+	// 	var otherX = T.Create(other.X);
+	// 	var otherY = T.Create(other.Y);
+	// 	
+	// 	vector.X = vector.X > otherX ? vector.X : otherX;
+	// 	vector.Y = vector.Y > otherY ? vector.Y : otherY;
+	//
+	// 	return ref vector;
+	// }
+
+	/*	
+	 * 
+	 * 	public static {returnType} {functionName}<{functionGenerics}>({functionParameters}) {functionGenericsDescription}
+	 * 	{
+	 *		{functionBody}
+	 *		return {returnValue};
+	 * 	}
+	 */
+
+	// public static double Cross<T, TOther, TDest>(this Vector2<T> vector, Vector2<TOther> other, ref Vector2<TDest> destination)
+	// 	where T : struct, INumber<T> where TOther : struct, INumber<TOther> where TDest : struct, INumber<TDest>
+	// {
+	// 	return vector.X * T.Create(other.X) + vector.Y * T.Create(other.Y);
+	// }
+
+	// public static ref Vector2<TDest> Add<T, TOther, TDest>(this Vector2<T> vector, Vector2<TOther> other, ref Vector2<TDest> destination)
+	// 	where T : struct, INumber<T> where TOther : struct, INumber<TOther> where TDest : struct, INumber<TDest> =>
+	// 	ref destination.Set(vector.Cast<T, TOther>() + (other));
+
+	// public static ref Vector2<T> Add<T>(this Vector2<T> vector, T value, ref Vector2<T> destination) where T : struct, INumber<T> => ref destination.Set(vector + value);
+
+	// public static ref Vector2<T> Add<T>(this Vector2<T> vector, Vector2<T> other, ref Vector2<T> destination) where T : struct, INumber<T> => ref destination.Set(vector + other);
+
+	// public static ref Vector3<T> Sub<T>(this ref Vector3<T> vector, T x, T y, T z) where T : struct, INumber<T>
+	// {
+	// 	vector.X -= x;
+	// 	vector.Y -= y;
+	// 	vector.Z -= z;
+	//
+	// 	return ref vector;
+	// }
+}
