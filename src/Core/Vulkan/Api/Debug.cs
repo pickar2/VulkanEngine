78cd66c0c04a481a78219e99560bf38bb795a41e
pickar <sereges2@gmail.com>
2022-08-07 13:57:02 +0300

    String manager and debug utility classes.

---
+++ src/Core/Vulkan/Api/Debug.cs
@@ -0,0 +1,68 @@
+﻿using Silk.NET.Vulkan;
+
+namespace Core.Vulkan.Api;
+
+public static unsafe class Debug
+{
+	public static void BeginQueueLabel(Queue queue, string name)
+	{
+		if (!Context.IsDebug) return;
+
+		var label = new DebugUtilsLabelEXT
+		{
+			SType = StructureType.DebugUtilsLabelExt,
+			PLabelName = StringManager.GetStringPtr<byte>(name)
+		};
+		Context.ExtDebugUtils.QueueBeginDebugUtilsLabel(queue, label);
+	}
+
+	public static void EndQueueLabel(Queue queue)
+	{
+		if (!Context.IsDebug) return;
+
+		Context.ExtDebugUtils.QueueEndDebugUtilsLabel(queue);
+	}
+
+	public static void InsertQueueLabel(Queue queue, string name)
+	{
+		if (!Context.IsDebug) return;
+
+		var label = new DebugUtilsLabelEXT
+		{
+			SType = StructureType.DebugUtilsLabelExt,
+			PLabelName = StringManager.GetStringPtr<byte>(name)
+		};
+		Context.ExtDebugUtils.QueueInsertDebugUtilsLabel(queue, label);
+	}
+
+	public static void BeginCmdLabel(CommandBuffer cmd, string name)
+	{
+		if (!Context.IsDebug) return;
+
+		var label = new DebugUtilsLabelEXT
+		{
+			SType = StructureType.DebugUtilsLabelExt,
+			PLabelName = StringManager.GetStringPtr<byte>(name)
+		};
+		Context.ExtDebugUtils.CmdBeginDebugUtilsLabel(cmd, label);
+	}
+
+	public static void EndCmdLabel(CommandBuffer cmd)
+	{
+		if (!Context.IsDebug) return;
+
+		Context.ExtDebugUtils.CmdEndDebugUtilsLabel(cmd);
+	}
+
+	public static void InsertCmdLabel(CommandBuffer cmd, string name)
+	{
+		if (!Context.IsDebug) return;
+
+		var label = new DebugUtilsLabelEXT
+		{
+			SType = StructureType.DebugUtilsLabelExt,
+			PLabelName = StringManager.GetStringPtr<byte>(name)
+		};
+		Context.ExtDebugUtils.CmdInsertDebugUtilsLabel(cmd, label);
+	}
+}
---
+++ src/Core/Vulkan/Api/StringManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Silk.NET.Core.Native;
+
+namespace Core.Vulkan.Api;
+
+public static unsafe class StringManager
+{
+	private static readonly Dictionary<string, nint> CachedStrings = new();
+
+	static StringManager() =>
+		Context.ContextEvents.AfterDispose += (() =>
+		{
+			foreach ((string? _, var ptr) in CachedStrings) SilkMarshal.Free(ptr);
+			CachedStrings.Clear();
+		});
+
+	public static nint GetStringPtr(string str)
+	{
+		if (CachedStrings.TryGetValue(str, out var ptr)) return ptr;
+		return CachedStrings[str] = SilkMarshal.StringToPtr(str);
+	}
+
+	public static T* GetStringPtr<T>(string str) where T : unmanaged => (T*) GetStringPtr(str);
+}
